9a63f0d24460f57693e929623432adcc28463e16
Adam Stachura <behir92@gmail.com>
2017-04-05 21:02:46 +0200

    tweaked the views, added book deletion from shelf

--- rpg_bookshelf/library/forms.py
+++ rpg_bookshelf/library/forms.py
@@ -1,36 +1,6 @@
 from django import forms
-from profiles.models import Profile
-from django.contrib.auth.hashers import make_password
-from django.contrib.auth.models import User
-from django.contrib.auth import authenticate
-
-class RegisterProfileForm(forms.ModelForm):
-
-    class Meta:
-        model = User
-        fields = ['username', 'email', 'password']
-
-    def __init__(self,*args,**kwargs):
-        super().__init__(*args,**kwargs)
-        self.fields['password'].widget = forms.PasswordInput()
-
-    def clean(self):
-        cleaned_data = super().clean()
-        raw_password = cleaned_data['password']
-        cleaned_data['password'] = make_password(raw_password)
-        return cleaned_data
+from library.models import Book
 
 
-class AuthForm(forms.Form):
-    login = forms.CharField(label="Login")
-    password = forms.CharField(label="Hasło", widget=forms.PasswordInput)
-
-    def clean(self):
-        cleaned_data = super().clean()
-        login = cleaned_data['login']
-        password = cleaned_data['password']
-        user = authenticate(username=login, password=password)
-        if user is None:
-            raise forms.ValidationError("Błędny login lub hasło")
-        cleaned_data['user'] = user
-        return cleaned_data
+class SearchForm(forms.Form):
+    title = forms.CharField(label="Tytuł podręcznika", max_length=128)
--- rpg_bookshelf/library/views.py
+++ rpg_bookshelf/library/views.py
@@ -1,20 +1,44 @@
 from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
-from django.shortcuts import render
+from django.contrib.auth.models import User
+from django.shortcuts import render,redirect
+from .forms import SearchForm
 from .models import (
     Book,
     Author,
     Publisher,
-    System
+    System,
+    Shelf,
 )
 from django.views import View
 from django.views.generic.edit import UpdateView, DeleteView, CreateView
 
 # Create your views here.
 
+class IndexView(View):
+    def get(self,request):
+        search_form = SearchForm()
+        ctx = {
+            'search_form': search_form
+        }
+        return render(request,'library/start.html', ctx)
+
+    def post(self,request):
+        search_form = SearchForm(data=request.POST)
+        ctx = {
+            'search_form': search_form
+        }
+        if search_form.is_valid():
+            title  = search_form.cleaned_data['title']
+            books = Book.objects.filter(title__icontains=title)
+            ctx['results'] = books
+        return render(request, 'library/search_results.html', ctx)
+
 class SystemListView(View):
     def get(self,request):
+        search_form = SearchForm()
         systems = System.objects.all().order_by('name')
-        ctx = {'systems': systems}
+        ctx = {'systems': systems,
+               'search_form': search_form}
         return render(request, 'library/system_list.html', ctx)
 
 class SystemView(View):
@@ -100,3 +124,49 @@ class ChangeBookView(PermissionRequiredMixin,UpdateView):
     permission_required = ['library.change_book']
     model = Book
     fields = '__all__'
+
+class SearchView(View):
+
+    def get(self,request):
+        form = SearchForm()
+        ctx = {
+            'form': form
+        }
+        return render(request,'library/search_results.html', ctx)
+
+    def post(self,request):
+        form = SearchForm(data=request.POST)
+        ctx = {
+            'form': form
+        }
+        if form.is_valid():
+            title  = form.cleaned_data['title']
+            books = Book.objects.filter(title__icontains=title)
+            ctx['results'] = books
+        return render(request, 'library/search_results.html', ctx)
+
+class ShelfView(View):
+    def get(self,request,user_id):
+        user = User.objects.get(id=user_id)
+        shelf = Shelf.objects.filter(user=user).order_by('book')
+        ctx = {
+            'shelf': shelf,
+            'user': user
+        }
+        return render(request, 'library/shelf.html', ctx)
+
+class AddBookToShelfView(View):
+    def get(self,request,book_id):
+        user = request.user
+        book = Book.objects.get(id=book_id)
+        shelf = Shelf.objects.create(user=user, book=book)
+        ctx = {
+            'user': user,
+            'book': book,
+            'shelf': shelf
+        }
+        return redirect('../../shelf/{}'.format(user.id))
+
+class DeleteBookFromShelf(DeleteView):
+    model = Shelf
+    success_url = '/'
